4aa1f6a8934d116372b880c7f8ec2c6884e0cf67
Jose David Gale Torre <josedavidgale@gmail.com>
2013-05-13 00:21:28 -0600

    Prueba
    Prueba

---
+++ Control1/Control1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Control1
+{
+    partial class Control1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblDisplay.Location = new System.Drawing.Point(46, 26);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 24);
+            this.lblDisplay.TabIndex = 0;
+            this.lblDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblDisplay.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Control1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblDisplay);
+            this.Name = "Control1";
+            this.Size = new System.Drawing.Size(269, 86);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Control1/Control1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Control1
+{
+    public partial class Control1 : UserControl
+    {
+
+        private System.Drawing.Color ColFColor;
+        private System.Drawing.Color ColBack;
+
+
+
+        public Control1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDisplay.Text = string.Format("HH:mm:ss", DateTime.Now);
+
+        }
+
+
+        //Declaramos la propiedad para el colFuente
+        public System.Drawing.Color ColorFuente
+        {
+            get
+            {
+                return ColFColor;
+            }
+            set
+            {
+                ColFColor = value;
+                lblDisplay.ForeColor = ColFColor;
+
+            }
+
+
+        }
+    }
+
+}
